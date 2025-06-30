f04c46c0436c8bd0c5249e977273067a8d60f06a
Sai Donepudi <saidonepudi2004@gmail.com>
2025-06-30 12:00:34 -0500

    removed toggle

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-AOS.init();
+// AOS.init();
 
-const toggle = document.querySelector('.theme-toggle');
-const body = document.body;
+// const toggle = document.querySelector('.theme-toggle');
+// const body = document.body;
 
-toggle.addEventListener('click', () => {
-  body.classList.toggle('dark');
-  localStorage.setItem('theme', body.classList.contains('dark') ? 'dark' : 'light');
-});
+// toggle.addEventListener('click', () => {
+//   body.classList.toggle('dark');
+//   localStorage.setItem('theme', body.classList.contains('dark') ? 'dark' : 'light');
+// });
 
-window.onload = () => {
-  const saved = localStorage.getItem('theme');
-  if (saved === 'dark') body.classList.add('dark');
-};
+// window.onload = () => {
+//   const saved = localStorage.getItem('theme');
+//   if (saved === 'dark') body.classList.add('dark');
+// };
